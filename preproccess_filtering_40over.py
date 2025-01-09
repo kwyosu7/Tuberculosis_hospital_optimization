8751df70680e71c5f067073582743cfcaecd3ee2
kwyosu7 <kwyosu27@gmail.com>
2025-01-09 21:49:47 +0900

    edit preproccess

--- preproccess_filtering_40over.py
+++ preproccess_filtering_40over.py
@@ -11,7 +11,7 @@ plt.rcParams['font.family'] = 'Helvetica'
 for year in range(2014, 2023):
 	path = '/home/users/YongsungKwon/workplace/Yongpyter/dataset/tuberculosis/data/DSL_data/' + str(year) + '.txt'
 	data = pd.read_csv(path, sep='\t')
-	
+	data = data[data['RD40_49'] != 0]
 	eta = data['h']/data['A']
 	N_40 = data['N'] * data['RN40_49']
 	D_40 = data['D'] * data['RD40_49']
@@ -21,6 +21,12 @@ for year in range(2014, 2023):
 	data['eta_tilde0_49'] = eta_tilde_40
 	# 컬럼 이름 변경
 	data.rename(columns={'eta_tilde0_49': 'eta_tilde40_49'}, inplace=True)
+	# 삭제할 컬럼 지정
+	columns_to_delete = ['eta_noage', 'eta_age_simul', 'eta_age_th']
+	# 컬럼 삭제
+	data.drop(columns=columns_to_delete, inplace=True)
 	save_path = '/home/users/YongsungKwon/workplace/Yongpyter/Tuberculosis_hospital_optimization/data_result/dataframe_40/' + str(year) + '_40.txt'
 	data.to_csv(save_path, index=False)
-	# %%
+# %%
+data
+# %%
