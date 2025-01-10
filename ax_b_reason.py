181dd5bb3fb4dc078f31df7118d2ea5412fb6fec
kwyosu7 <kwyosu27@gmail.com>
2025-01-10 16:02:08 +0900

    edit at+b. it is not working

--- ax_b_reason.py
+++ ax_b_reason.py
@@ -9,22 +9,26 @@ from matplotlib.gridspec import GridSpec
 plt.rcParams['font.family'] = 'Helvetica'
 #%%
 for year in range(2014, 2023):
-    path_N='/home/users/YongsungKwon/workplace/Yongpyter/dataset/tuberculosis/data/2023_rate_extract_DSL/'+str(year)+'_extract_N.csv'
-    path_D='/home/users/YongsungKwon/workplace/Yongpyter/dataset/tuberculosis/data/2023_rate_extract_DSL/'+str(year)+'_extract_D.csv'
-    path='/home/users/YongsungKwon/workplace/Yongpyter/dataset/tuberculosis/data/DSL_data/'+str(year)+'.txt'
-    data_age_N = pd.read_csv(path_N)
-    data_age_D = pd.read_csv(path_D)
-    data = pd.read_csv(path,sep='\t')
-    RNage_list_49 = ['RN0_9', 'RN10_19', 'RN20_29', 'RN30_39', 'RN40_49']
-    RN = [np.mean(data[RNage_list_49].sum(axis=1))]
-    for t in ['50_59', '60_69', '70_79', '80_']:
-        RN.append(np.mean(data['RN'+t]))
+    # path_N='/home/users/YongsungKwon/workplace/Yongpyter/dataset/tuberculosis/data/2023_rate_extract_DSL/'+str(year)+'_extract_N.csv'
+    # path_D='/home/users/YongsungKwon/workplace/Yongpyter/dataset/tuberculosis/data/2023_rate_extract_DSL/'+str(year)+'_extract_D.csv'
+    path='/home/users/YongsungKwon/workplace/Yongpyter/Tuberculosis_hospital_optimization/data_result/dataframe_40/'+str(year)+'_40.txt'
+    # data_age_N = pd.read_csv(path_N)
+    # data_age_D = pd.read_csv(path_D)
+    data = pd.read_csv(path,sep=',')
+    # RNage_list_49 = ['RN0_9', 'RN10_19', 'RN20_29', 'RN30_39', 'RN40_49']
+    RN = []
+    D = []
+    for t in ['40_49', '50_59', '60_69', '70_79', '80_']:
+        # RN.append(np.mean(data['N']*data['RN'+t]))
+        # RN.append(sum(data['N']*data['RN'+t]))
+        D.append(np.mean(data['D']*data['RD'+t]))
     phi=[]
-    N_49 = data['N']* data[RNage_list_49].sum(axis=1)
-    for t in ['0_49', '50_59', '60_69', '70_79', '80_']:
+    
+    # N_49 = data['N']* data[RNage_list_49].sum(axis=1)
+    for t in ['40_49', '50_59', '60_69', '70_79', '80_']:
             eta_i = data['h']/data['A']
-            if t=='0_49': N_it=N_49
-            else: N_it=data['N']*data['RN'+t]
+            # if t=='0_49': N_it=N_49
+            N_it=data['N']*data['RN'+t]
             phi.append(np.mean(np.exp(-eta_i/data['eta_tilde'+t])))
 
 
@@ -35,13 +39,15 @@ for year in range(2014, 2023):
     gs = GridSpec(1, 2, figure=fig)
     # 첫 번째 서브플롯
     ax1 = fig.add_subplot(gs[0, 0])  # 첫 번째 열
-    ax1.plot(['0_49', '50_59', '60_69', '70_79', '80_'], RN, marker='o')  # 샘플 데이터
-    ax1.set_ylabel(r'$\langle N\rangle_i$',size=20)
+    # ax1.plot(['40_49', '50_59', '60_69', '70_79', '80_'], RN, marker='o')  # 샘플 데이터
+    ax1.plot(['40_49', '50_59', '60_69', '70_79', '80_'], D, marker='o')  # 샘플 데이터
+    # ax1.set_ylabel(r'$\langle N\rangle_i$',size=20)
+    ax1.set_ylabel(r'$\langle D\rangle_i$',size=20)
     ax1.set_xlabel('age',size=20)
-    ax1.set_ylim(0.14, 0.41)
+    # ax1.set_ylim(0, 41)
     # 두 번째 서브플롯
     ax2 = fig.add_subplot(gs[0, 1])  # 두 번째 열
-    ax2.plot(['0_49', '50_59', '60_69', '70_79', '80_'], phi, marker='o') 
+    ax2.plot(['40_49', '50_59', '60_69', '70_79', '80_'], phi, marker='o') 
     ax2.set_ylabel(r'$\langle \phi\rangle_i$',size=20)
     ax2.set_xlabel('age',size=20)
     ax2.set_ylim(0,0.25)
@@ -52,7 +58,7 @@ for year in range(2014, 2023):
 # %%
 
 """
-0~29, 30~49
+over 40 
 """
 import numpy as np
 import pandas as pd
@@ -148,4 +154,6 @@ for year in range(2014, 2023):
     columns = ['RD0_9', 'RD10_19', 'RD20_29', 'RD30_39', 'RD40_49']
     zero_counts = {col: (data[col] == 0).sum() for col in columns}
     print(zero_counts)
+# %%
+data
 # %%
--- function_ax_b.py
+++ function_ax_b.py
@@ -8,15 +8,10 @@ plt.rcParams['mathtext.fontset'] = 'cm'
 plt.rcParams['font.family'] = 'Helvetica'
 import sys
 #%%
-year=int(sys.argv[1])#2014
-# path_N='/home/users/YongsungKwon/workplace/Yongpyter/dataset/tuberculosis/data/2023_rate_extract_DSL/'+str(year)+'_extract_N.csv'
-# path_D='/home/users/YongsungKwon/workplace/Yongpyter/dataset/tuberculosis/data/2023_rate_extract_DSL/'+str(year)+'_extract_D.csv'
-# path='/home/users/YongsungKwon/workplace/Yongpyter/dataset/tuberculosis/data/DSL_data/'+str(year)+'.txt'
-# data_age_N = pd.read_csv(path_N)
-# data_age_D = pd.read_csv(path_D)
+year=2014#int(sys.argv[1])#2014
 path = '/home/users/YongsungKwon/workplace/Yongpyter/Tuberculosis_hospital_optimization/data_result/dataframe_40/'+str(year)+'_40.txt'
 data = pd.read_csv(path,sep=',')
-data['h_opt_age'] = data['h']
+# data['h_opt_age'] = data['h']
 
 #%%
 # data
@@ -33,39 +28,27 @@ def E(data, opt=True):
         E+=N_it*phi_it
     return sum(E)
 
-# def E_optimization():
-#     eta_i = data['eta_age_simul']
-#     RNage_list_49= ['RN0_9', 'RN10_19', 'RN20_29', 'RN30_39', 'RN40_49']
-#     N_49 = data['N']* data[RNage_list_49].sum(axis=1)
-#     E=0
-#     for t in ['0_49', '50_59', '60_69', '70_79', '80_']:
-#         if t=='0_49': N_it=N_49
-#         else: N_it=data['N']*data['RN'+t]
-#         phi_it= np.exp(-eta_i/data['eta_tilde'+t])
-#         E+=N_it*phi_it
-#     return sum(E)
-
-def consideration_weight_b(a, data):
-    E_0 = sum(data['D'])
+def consideration_weight_b(a, data_):
+    E_0 = sum(data_['D'])
     t_list = [45, 55, 65, 75, 85]
-    eta_i = data['h']/data['A']
+    eta_i = data_['h']/data_['A']
     E_a,c=0,0
     for t in ['40_49', '50_59', '60_69', '70_79', '80_']:
-        N_it=data['N']*data['RN'+t]
-        phi_it= np.exp(-eta_i/data['eta_tilde'+t])
+        N_it=data_['N']*data_['RN'+t]
+        phi_it= np.exp(-eta_i/data_['eta_tilde'+t])
         E_a+=(a*t_list[c]) * N_it*phi_it 
         c+=1
     return (E_0-sum(E_a))/(E_0) # b
 
-def E_consideratoin_weight(a, b, data, opt=True):
+def E_consideratoin_weight(a, b, data_, h_opt_):
     t_list = [45, 55, 65, 75, 85]
-    if opt:eta_i = data['h_opt_age']/data['A']
-    else:eta_i = data['h']/data['A']
+    eta_i = h_opt_['h']/data_['A']
+    # eta_i = data['h']/data['A']
     
     E,c=0,0
     for t in ['40_49', '50_59', '60_69', '70_79', '80_']:
-        N_it=data['N']*data['RN'+t]
-        phi_it= np.exp(-eta_i/data['eta_tilde'+t])
+        N_it=data_['N']*data_['RN'+t]
+        phi_it= np.exp(-eta_i/data_['eta_tilde'+t])
         E+=(a*t_list[c]+b)*N_it*phi_it 
         c+=1
     return sum(E)
@@ -76,34 +59,36 @@ b=[]
 ai=0
 for a in np.linspace(0,0.013, 10):
     dh = 0.01
+    h_opt = pd.DataFrame(data['h'].copy())
     b=consideration_weight_b(a, data)
     index_list = list(data.index)
     E_list=[]
-    Ei = E_consideratoin_weight(a,b,data)
+    Ei = E_consideratoin_weight(a,b,data,h_opt)
     c=0
-    for i in range(100000):
+    for i in range(1000):
         E_list.append(Ei)
         Rsigungu = random.sample(index_list, 2)
-        if data.loc[Rsigungu[1], 'h_opt_age'] <=0:pass
+        if h_opt.loc[Rsigungu[1], 'h'] <=0:pass
         else:
-            data.loc[Rsigungu[0], 'h_opt_age'] += dh
-            data.loc[Rsigungu[1], 'h_opt_age'] -= dh
-            Ef = E_consideratoin_weight(a,b,data)
+            h_opt.loc[Rsigungu[0], 'h'] += dh
+            h_opt.loc[Rsigungu[1], 'h'] -= dh
+            Ef = E_consideratoin_weight(a,b,data,h_opt)
             if Ei <= Ef:
-                data.loc[Rsigungu[0], 'h_opt_age'] -= dh
-                data.loc[Rsigungu[1], 'h_opt_age'] += dh
+                h_opt.loc[Rsigungu[0], 'h'] -= dh
+                h_opt.loc[Rsigungu[1], 'h'] += dh
             else:
                 Ei=Ef
                 c+=1
 
     save_path='/home/users/YongsungKwon/workplace/Yongpyter/Tuberculosis_hospital_optimization/data_result/at_b/opt_h_E/'
     E_list=np.array(E_list)
-    np.save(save_path+str(year)+'_age_E_a_'+str(ai)+'.npy',E_list,allow_pickle=True)
-    data.to_csv(path,index=False)
+    # np.save(save_path+str(year)+'_age_E_a_'+str(ai)+'.npy',E_list,allow_pickle=True)
+    # h_opt.to_csv(save_path+str(year)+'MC_age_h_opt_a_'+str(ai)+'.csv', index=False)
+    # data.to_csv(path,index=False)
     ai+=1
-# #%%
-# plt.plot(E_list)
-# # %%
+#%%
+# h_opt
+# %%
 # c
 # # %%
 # E_optimization()
@@ -120,39 +105,42 @@ for a in np.linspace(0,0.013, 10):
 # #%%
 # str(0.01)
 # %%
+# h_opt
+#%%
 """
 Check result
 """
-# for year in range(2014, 2023):
-#     save_path='/home/users/YongsungKwon/workplace/Yongpyter/Tuberculosis_hospital_optimization/data_result/at_b/opt_h_E/'
-#     # year=2016
-#     path_N='/home/users/YongsungKwon/workplace/Yongpyter/dataset/tuberculosis/data/2023_rate_extract_DSL/'+str(year)+'_extract_N.csv'
-#     path_D='/home/users/YongsungKwon/workplace/Yongpyter/dataset/tuberculosis/data/2023_rate_extract_DSL/'+str(year)+'_extract_D.csv'
-#     path='/home/users/YongsungKwon/workplace/Yongpyter/dataset/tuberculosis/data/DSL_data/'+str(year)+'.txt'
-#     data_age_N = pd.read_csv(path_N)
-#     data_age_D = pd.read_csv(path_D)
-#     data = pd.read_csv(path,sep='\t')
+for year in range(2014, 2023):
+    save_path='/home/users/YongsungKwon/workplace/Yongpyter/Tuberculosis_hospital_optimization/data_result/at_b/opt_h_E/'
+    # year=2016
+    # path_N='/home/users/YongsungKwon/workplace/Yongpyter/dataset/tuberculosis/data/2023_rate_extract_DSL/'+str(year)+'_extract_N.csv'
+    # path_D='/home/users/YongsungKwon/workplace/Yongpyter/dataset/tuberculosis/data/2023_rate_extract_DSL/'+str(year)+'_extract_D.csv'
+    path = '/home/users/YongsungKwon/workplace/Yongpyter/Tuberculosis_hospital_optimization/data_result/dataframe_40/'+str(year)+'_40.txt'
+    # data_age_N = pd.read_csv(path_N)
+    # data_age_D = pd.read_csv(path_D)
+    data = pd.read_csv(path,sep=',')
     
-#     age_list_49= ['0_9', '10_19', '20_29', '30_39', '40_49']
-#     Total_N_49=np.sum(data_age_N[age_list_49].sum(axis=1))
-#     N_age=0
-#     for a in age_list_49:
-#         N_age += np.sum(data_age_N[a])/Total_N_49*(int(a[0])*10+5)
+    # age_list_49= ['0_9', '10_19', '20_29', '30_39', '40_49']
+    # Total_N_49=np.sum(data_age_N[age_list_49].sum(axis=1))
+    # N_age=0
+    # for a in age_list_49:
+    #     N_age += np.sum(data_age_N[a])/Total_N_49*(int(a[0])*10+5)
     
-#     E_opt_list = []
-#     for i in range(10):
-#         a=np.linspace(0,0.013,10)[i]
-#         h_opt = pd.read_csv(save_path+str(year)+'MC_age_h_opt_a_'+str(i)+'.csv')
-#         b=consideration_weight_b(a, data)
-#         E_opt=E_consideratoin_weight(a,b,data,h_opt)
-#         E_opt_list.append(E_opt)
+    E_opt_list = []
+    for i in range(10):
+        a=np.linspace(0,0.013,10)[i]
+        h_opt = pd.read_csv(save_path+str(year)+'MC_age_h_opt_a_'+str(i)+'.csv',sep=',')
+        # print(h_opt)
+        b=consideration_weight_b(a, data)
+        E_opt=E_consideratoin_weight(a,b,data,h_opt)
+        E_opt_list.append(E_opt)
     
-#     plt.scatter(np.linspace(0,0.013,10), E_opt_list)
-#     plt.xlabel(r'$a$',size=20)
-#     plt.ylabel(r'$E^\mathrm{opt}$',size=20)
-#     plt.tight_layout()
-#     plt.savefig(save_path+str(year)+'a_E.pdf',format='pdf',transparent=True)
-#     plt.show()
+    plt.scatter(np.linspace(0,0.013,10), E_opt_list)
+    plt.xlabel(r'$a$',size=20)
+    plt.ylabel(r'$E^\mathrm{opt}$',size=20)
+    plt.tight_layout()
+    plt.savefig(save_path+str(year)+'a_E.pdf',format='pdf',transparent=True)
+    plt.show()
 # # %%
 # save_path='/home/users/YongsungKwon/workplace/Yongpyter/Tuberculosis_hospital_optimization/data_result/at_b/opt_h_E/'
 # # year=2016
@@ -173,3 +161,53 @@ Check result
 #     print(str(year), N_age)
 # # %%
 
+# E(data,opt=False)
+# # %%
+# E_opt_list = []
+# for i in range(10):
+#     a=np.linspace(0,0.013,10)[i]
+#     cos_sim = cosine_similarity([data['h']], [h_opt['h']])[0][0]
+#     print(f"코사인 유사도: {cos_sim}")
+#     h_opt = pd.read_csv(save_path+str(year)+'MC_age_h_opt_a_'+str(i)+'.csv',sep=',')
+#     E=E_consideratoin_weight(a, consideration_weight_b(a, data), data, data)
+#     print(E)
+#     E=E_consideratoin_weight(a, consideration_weight_b(a, data), data, h_opt)
+#     print(E)
+
+# # %%
+# year
+# # %%
+# data
+# # %%
+# h_opt
+# # %%
+# data['h']
+# # %%
+# from sklearn.metrics.pairwise import cosine_similarity
+# cos_sim = cosine_similarity([data['h']], [h_opt['h']])[0][0]
+# print(f"코사인 유사도: {cos_sim}")
+# # %%
+
+# %%
+b = []
+for a in np.linspace(0,0.015,100):
+    b.append(consideration_weight_b(a,data))
+plt.scatter(np.linspace(0,0.015,100), b)
+
+# %%
+eta_i = data['h']/data['A']
+    
+E=0
+for t in ['40_49', '50_59', '60_69', '70_79', '80_']:
+    N_it=data['N']*data['RN'+t]
+    phi_it= np.exp(-eta_i/data['eta_tilde'+t])
+    E+=N_it*phi_it
+#%%
+sum(E)
+# %%
+b=consideration_weight_b(0, data)
+E_opt=E_consideratoin_weight(0,b,data,data)
+E_opt
+# %%
+data
+# %%
